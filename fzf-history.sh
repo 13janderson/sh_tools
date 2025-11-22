474d4bcdcf70d128bbf35de78dfd06e0400939a9
Jack Anderson <jackanderson02@outlook.com>
2025-11-22 22:28:07 +0000

    feat: better multi select command chaining with &&

--- fzf-history.sh
+++ fzf-history.sh
@@ -6,7 +6,7 @@ TMP_FLAG=$(mktemp)
 # Change this for your shell, you may also need to change the below sed filtering to remove noise from history files
 # the below works for zsh_history
 HISTFILE=$HOME/.zsh_history
-selected=$(tac $HISTFILE | sed -E "/$me/d" | sed -E 's/(: [0-9]*:0;)(.*)/\2/' | awk '!seen[$0]++' | fzf -m --bind "ctrl-y:execute-silent(echo {} | xclip -selection clipboard; echo y > $TMP_FLAG)+accept")
+selected=$(tac $HISTFILE | sed -E "/$me/d" | sed -E 's/(: [0-9]*:0;)(.*)/\2/' | awk '!seen[$0]++' | fzf -m --bind "ctrl-y:execute-silent(echo {} | wl-copy; echo y > $TMP_FLAG)+accept")
 echo "$selected"
 
 if [[ -f "$TMP_FLAG" ]]; then
@@ -19,6 +19,8 @@ if [[ -f "$TMP_FLAG" ]]; then
 fi
 
 rm $TMP_FLAG > /dev/null 2>&1
+
+selected=$(echo "$selected" | awk '{printf "%s && ", $0}' | sed 's/&& $//')
 
 if [[ -n "$selected" ]]; then
   # Send keys to current tmux window and hit enter, I love tmux.
