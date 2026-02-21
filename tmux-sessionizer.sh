32b226df8bd3ceb57a50a9c3c68112859ea4f645
Jack Anderson <{"message":"Not Found","documentation_url":"https://docs.github.com/rest/users/emails#list-email-addresses-for-the-authenticated-user","status":"404"}>
2026-02-21 18:51:38 +0000

    feat: qol tmux improvements

--- tmux-sessionizer.sh
+++ tmux-sessionizer.sh
@@ -3,11 +3,33 @@ selected_name=$1
 
 custom_paths=("$HOME/vault/" "$HOME/dotfiles" )
 
+
+# When showing tmux sesions, I want them organised by latest first
 if [ -z $selected_name ]; then
   if [[ $# -eq 1 ]]; then
       selected=$1
   else
-    selected=$( (printf "%s\n" "${custom_paths[@]}"; find $HOME/projects  $HOME/projects/plugins/  $HOME/projects/CVS -mindepth 1 -maxdepth 1 -type d -not -path '*/.*') | sed -E "s|$HOME/||" |   fzf --preview '')
+    # mark active sessions with a *
+    sessions=$(tmux ls -F "#{session_activity}(#{session_name}" | sort -n | cut -d "(" -f 2) 
+    # echo "sessions: $sessions"
+    # times=$(echo "$sessions" | sed -E "s/(.*):(.*)/\1/" | xargs -I{} sh -c "echo $(($(date +%s) - $1)) | awk '{s=int($1/1000); d=int(s/86400); h=int((s%86400)/3600); m=int((s%3600)/60); printf \"%dd %dh %dm\n\", d,h,m}'" _ {})
+    # times=$(echo "$sessions" | sed -E 's/.*:(.*)/\1/' | \
+    #   awk -v now=$(date +%s) '{s=now-$1; d=int(s/86400); h=int((s%86400)/3600); m=int((s%3600)/60); printf "%dd %dh %dm\n", d,h,m}')
+    sessions=$(echo "$sessions" | sed -E "s/(.*):(.*)/\2/")
+    # echo "times: $times"
+    directories=$(printf "%s\n" "${custom_paths[@]}"; find $HOME/projects $HOME/projects/plugins/ $HOME/projects/CVS -mindepth 1 -maxdepth 1 -type d -not -path '*/.*' 2> /dev/null)
+    directories=$(echo "$directories" | sed -E "s|$HOME/||" | sed 's|/*$||')
+    # echo "directories: $directories"
+    # echo "sessions: $sessions"
+    existing=$(echo "$directories" | grep -wE "$sessions" | sed -E "s/(.*)/\1/")
+    # echo "existing: $existing"
+    # echo $(paste "$existing" "$times")
+    # echo "existing: $existing"
+    non_existing=$(echo "$directories" | grep -v "$existing")
+
+    existing_colour=$(echo "$existing" | sed -E $'s/(.*)/\e[1;92m\\1\e[0m/')
+    
+    selected=$(printf "$existing_colour\n$non_existing" | fzf --ansi --tmux 70%)
   fi
 
   if [[ -z $selected ]]; then
