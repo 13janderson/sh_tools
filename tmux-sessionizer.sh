1c07bcd0b707852ab2a062cc30bbc9ba3f6d6f9e
Jack Anderson <jack.anderson@bjss.com>
2026-05-06 10:53:54 +0100

    fix: tmux session cd

--- tmux-sessionizer.sh
+++ tmux-sessionizer.sh
@@ -12,8 +12,8 @@ if [ -z $selected_name ]; then
 
     directories=$(printf "%s\n" "${custom_paths[@]}"; find $HOME/projects $HOME/projects/plugins/ $HOME/projects/CVS -mindepth 1 -maxdepth 1 -type d -not -path '*/.*' 2> /dev/null)
 
-    # tmuxify all directories 
-    directories=$(echo "$directories" | xargs -I {} sh -c "echo {} | tr . _" | sed -E "s|$HOME/||" | sed 's|/*$||')
+    # Remove $HOME prefix and trailing slashes for display
+    directories=$(echo "$directories" | sed -E "s|$HOME/||" | sed 's|/*$||')
 
     existing=$(echo "$directories" | grep -wE "$sessions" | sed -E "s/(.*)/\1/")
     if [[ -n $existing ]]; then
@@ -22,7 +22,7 @@ if [ -z $selected_name ]; then
       existing_colour=$(echo "$existing" | sed -E $'s/(.*)/\e[1;92m\\1\e[0m/')
 
       # Allow multiple selections when sessions plural already exist
-      selected=$(printf "$existing_colour\n$non_existing" | tr _ . | fzf -m --ansi --tmux 70%)
+      selected=$(printf "$existing_colour\n$non_existing" | fzf -m --ansi --tmux 70%)
 
       # Assume that the use case for this is selecting multiple sessions for deletion. Thus assume
       # that when multiple sessions are selected, then the intention is to delete those sessions
@@ -33,7 +33,7 @@ if [ -z $selected_name ]; then
           printf "Deleting sessions: $(echo "$selected" | tr '\n' ' ')\n" && echo "$selected" | xargs -I {} sh -c 'tmux kill-session -t "$(basename "{}" | tr . _ )"'
       fi
     else
-      selected=$(printf "$directories" | tr . _ | fzf --ansi --tmux 70%)
+      selected=$(printf "$directories" | fzf --ansi --tmux 70%)
     fi
   fi
 
@@ -41,6 +41,7 @@ if [ -z $selected_name ]; then
       exit 0
   fi
 
+  # Session name uses underscores instead of dots (tmux session naming restriction)
   selected_name=$(basename "$selected" | tr . _)
   tmux_running=$(pgrep tmux)
 fi
@@ -48,13 +49,13 @@ fi
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
     pth="$HOME/$selected"
 
-    tmux new-session -s $selected_name -c $pth
+    tmux new-session -s "$selected_name" -c "$pth"
     exit 0
 fi
 
-if ! tmux has-session -t=$selected_name 2> /dev/null; then
+if ! tmux has-session -t="$selected_name" 2> /dev/null; then
     pth="$HOME/$selected"
-    tmux new-session -ds $selected_name -c $pth
+    tmux new-session -ds "$selected_name" -c "$pth"
 fi
 
 if [[ -z $TMUX ]]; then
